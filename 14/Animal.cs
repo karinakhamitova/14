81cda07ec49a3e4e6e8a4f04cc1c681b5517c9b1
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-11-14 20:01:28 +0300

    Добавьте файлы проекта.

---
+++ 14/Animal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name)
+           :base(name)
+        {
+            this.name = name;
+        }
+   
+        public override void Say()
+        {
+            Console.WriteLine("Myr-myr");
+        }
+    }
+    class Dog : Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name)
+           : base(name)
+        {
+            this.name = name;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Gav-gav");
+        }
+    }
+}
---
+++ 14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Gav");
+            Dog dog = new Dog("Bim");
+            cat.ShowInfo();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
